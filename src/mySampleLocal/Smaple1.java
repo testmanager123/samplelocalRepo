e5460fecf39dd44d02c449165cd3936f37b286e6
srinivasa <srinivasavy@gmail.com>
2020-04-12 13:16:01 +0530

    initial commit

---
+++ src/mySampleLocal/Smaple1.java
@@ -0,0 +1,11 @@
+package mySampleLocal;
+
+public class Smaple1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is created first time");
+
+	}
+
+}
---
+++ src/mySampleLocal/myTestSma.java
@@ -0,0 +1,11 @@
+package mySampleLocal;
+
+public class myTestSma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("third one");
+
+	}
+
+}
